3c8c66a23d7e3e7e79a6ec23e823b7c85e9b7772
Jhon Jairo Urueta Alvarez <jhon.urueta@misena.edu.co>
2023-10-11 00:01:42 -0500

    Avance 14

--- src/config/global.js
+++ src/config/global.js
@@ -116,7 +116,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/331502_CF21_DU.zip',
+        download: 'downloads/331502_CF21_DU.pdf',
       },
       {
         icono: 'fas fa-download',
